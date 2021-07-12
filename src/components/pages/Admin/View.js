02e86bc3d71d9184ef81fbb70b90fcef48bde87c
EverytimeAomsin <sarin542@hotmail.com>
2021-07-12 15:56:29 +0700

    เก็บงาน view โชว์ข้อความ

--- src/components/pages/Admin/View.js
+++ src/components/pages/Admin/View.js
@@ -2,99 +2,102 @@ import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { useHistory, Link, useParams } from "react-router-dom";
 import DownloadLink from "react-download-link";
+
 import "../../../css/showimg.css"
 import "../../../css/View.css"
 
-const View = ({ menus}) => {
-    const { id } = useParams();
-    const history = useHistory();
-    const currentMenu = menus.find(
-        (menu) => menu.id === parseInt(id)
-    );
+const View = ({ menus }) => {
+  const { id } = useParams();
+  const history = useHistory();
+  const currentMenu = menus.find(
+    (menu) => menu.id === parseInt(id)
+  );
 
-    useEffect(() => {document.title = "ร้าน "+(currentMenu.name)
-        setName(currentMenu.name);
-        setDescription(currentMenu.description);
-        setQr(currentMenu.qr);
-        setImg(currentMenu.img);
-    }, [currentMenu]);
+  useEffect(() => {
+    document.title = "ร้าน " + (currentMenu.name)
+    setName(currentMenu.name);
+    setDescription(currentMenu.description);
+    setQr(currentMenu.qr);
+    setImg(currentMenu.img);
+  }, [currentMenu]);
 
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
-    const [qr, setQr] = useState("");
-    const [img, setImg] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [qr, setQr] = useState("");
+  const [img, setImg] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-       
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        const data = {
-            id: currentMenu.id,
-            description,
-            name,
-            img,
-            qr,
-        };
+
+    const data = {
+      id: currentMenu.id,
+      description,
+      name,
+      img,
+      qr,
     };
+  };
 
-    return (
+  return (
 
 
-       <div>
+    <div>
 
-            <div>
-      <form onSubmit={handleSubmit}>
-        <div className="header">
-          <h1>View</h1>
-        </div>
-        
-        <div className="row">
-
-          <div className="col-6 col-s-12 menu">
-            <ul>
-              
-                <h4 style={{ textAlign:"left", marginLeft:'30px'}}>ชื่อ : {name} </h4>
-              
-              <h4 style={{ textAlign:"left", marginLeft:'30px',marginTop:'8px'}}>Description : {description}</h4> 
-              <h4 style={{ textAlign:"left", marginLeft:'30px',marginTop:'8px'}}>ที่อยู่ไฟล์รูป : {img}</h4>
-              <h4 style={{ textAlign:"left", marginLeft:'30px',marginTop:'8px'}}>ที่อยู่ไฟล์ QR : {qr}</h4> 
-
-            </ul>
-          </div>
-          <div className="col-6 col-s-12">
-
-            <div className="flex-parent  jc-center">
-            <img
-                                src={img}
-                                onChange={(e) => setImg(e.target.value)}
-                            />
-                            <img
-                                src={img}
-                                onChange={(e) => setImg(e.target.value)}
-                            />
-            </div>
-
-            <div className="flex-parentbt  jc-center " style={{marginTop:'15px'}}>
-              <button className="button button1" style={{marginLeft:'10px'}} type="button" > โหลด QR-CODE</button>
-              <Link to={`/admin`}><button className="button button3">กลับหน้าผู้ดูแล</button></Link>
-
-            </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="header">
+            <h1>View</h1>
           </div>
 
-        </div>
-      </form>
+          <div className="row">
+
+            <div className="col-6 col-s-12 menu">
+              <ul>
+
+                <div className="showtext"><p style={{ fontWeight: 'bold' }}>ชื่อ :</p> <p>{name} </p></div>
+                <div className="showtext"><p style={{ fontWeight: 'bold' }}>Description :</p> <p style={{ display: 'block' }}>{description} </p></div>
+                <div className="showtext"><p style={{ fontWeight: 'bold' }}>ที่อยู่ไฟล์รูป :</p> <p>{img} </p></div>
+                <div className="showtext"><p style={{ fontWeight: 'bold' }}>ที่อยู่ไฟล์ QR :</p> <p>{qr} </p></div>
+
+
+
+              </ul>
+            </div>
+            <div className="col-6 col-s-12">
+
+              <div className="flex-parent  jc-center">
+                <img
+                  src={img}
+                  onChange={(e) => setImg(e.target.value)}
+                />
+                <img
+                  src={img}
+                  onChange={(e) => setImg(e.target.value)}
+                />
+              </div>
+
+              <div className="flex-parentbt  jc-center " style={{ marginTop: '15px' }}>
+                <button className="button button1" style={{ marginLeft: '10px' }} type="button" > โหลด QR-CODE</button>
+                <Link to={`/admin`}><button className="button button3">กลับหน้าผู้ดูแล</button></Link>
+
+              </div>
+            </div>
+
+          </div>
+        </form>
+      </div>
     </div>
-        </div>
-    );
+  );
 };
 
 const mapStateToProps = (state) => ({
-    menus: state,
+  menus: state,
 });
 const mapDispatchToProps = (dispatch) => ({
-    updatemenu: (data) => {
-        dispatch({ type: "UPDATE_menu", payload: data });
-    },
+  updatemenu: (data) => {
+    dispatch({ type: "UPDATE_menu", payload: data });
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(View);
