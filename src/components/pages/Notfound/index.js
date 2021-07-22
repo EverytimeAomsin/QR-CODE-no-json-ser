46fefe78972277e5d16ebb5c6799ba8563bc14ec
EverytimeAomsin <sarin542@hotmail.com>
2021-07-22 14:59:31 +0700

    ก่อนเปลียน Ui

--- src/components/pages/Notfound/index.js
+++ src/components/pages/Notfound/index.js
@@ -4,6 +4,7 @@ import React from 'react'
 function NotFound() {
     return (
         <div ><h1 className="text-center " style={{ marginTop: '80px' }}><div>ขออภัย</div>ไม่พบหน้าที่คุณต้องการ</h1></div>
+        
     )
 }
 
