c1d563815b7d38e6ee991240dacf942867ad438d
EverytimeAomsin <sarin542@hotmail.com>
2021-07-24 05:05:01 +0700

    แก้ css

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -10,7 +10,7 @@ const Home = ({menus }) => {
      <h1 className="head-center " style={{backgroundColor:'transparent',fontSize:'230%'}}>ร้านค้าที่เปิดให้บริการ</h1>
         {menus.map((menu) => (
                  <div className="column">
-                 <div className="content card shadowv" >
+                 <div className="contentH cardH shadowv" >
                  <Link   to={`/${menu.id}`} ><img className="img-fluid "  src={menu.img} /></Link>
                    <Link  to={`/${menu.id}`} style={{textDecoration:"none"}}><h2 >{menu.name}</h2></Link>
                    <Link  to={`/${menu.id}`} style={{textDecoration:"none"}}><p>{menu.description}</p></Link>
